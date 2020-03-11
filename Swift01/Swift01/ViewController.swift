e447b7483d9c096e54320d2706bf133122be728f
lusir <luzhang316@gmail.com>
2020-03-11 18:39:15 +0800

    Swift01 project init

---
+++ Swift01/Swift01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift01
+//
+//  Created by da ghua on 2020/3/11.
+//  Copyright © 2020 da ghua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift01/Swift01Tests/Swift01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift01Tests.swift
+//  Swift01Tests
+//
+//  Created by da ghua on 2020/3/11.
+//  Copyright © 2020 da ghua. All rights reserved.
+//
+
+import XCTest
+@testable import Swift01
+
+class Swift01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
