d7600df0bf8bc97987db70893d215031314f1297
lusir <luzhang316@gmail.com>
2020-03-14 11:33:27 +0800

    swift02 Stopwatch init project

---
+++ Swift02-Stopwatch/Swift02-Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift02-Stopwatch
+//
+//  Created by da ghua on 2020/3/14.
+//  Copyright © 2020 da ghua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift02-Stopwatch/Swift02-StopwatchTests/Swift02_StopwatchTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift02_StopwatchTests.swift
+//  Swift02-StopwatchTests
+//
+//  Created by da ghua on 2020/3/14.
+//  Copyright © 2020 da ghua. All rights reserved.
+//
+
+import XCTest
+@testable import Swift02_Stopwatch
+
+class Swift02_StopwatchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
