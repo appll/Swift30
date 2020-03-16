4c5fe8df7730d6ba1a0ae60fcc87f1412e27b410
lusir <luzhang316@gmail.com>
2020-03-16 14:35:07 +0800

    使用snapkit适配流海屏

--- Swift01/Swift01/UsViewController.swift
+++ Swift01/Swift01/UsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class UsViewController: UIViewController {
 
@@ -17,19 +18,30 @@ class UsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         view.addSubview(scrollView)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        if #available(iOS 11, *) {
-            let safeTop = view.safeAreaInsets.top
-            let safeBottom = view.safeAreaInsets.bottom
-            scrollView.frame = CGRect(x: 0, y: safeTop, width: view.frame.width, height: view.frame.height - safeBottom - safeTop)
-        } else {
-            let safeTop = topLayoutGuide.length
-            let safeBottom = bottomLayoutGuide.length
-            scrollView.frame = CGRect(x: 0, y: safeTop, width: view.frame.width, height: view.frame.height - safeBottom - safeTop)
+        scrollView.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview()
+            if #available(iOS 11, *) {
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+                make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            } else {
+                make.top.equalTo(self.topLayoutGuide.snp.bottom)
+                make.bottom.equalTo(self.bottomLayoutGuide.snp.top)
+            }
         }
         scrollView.contentSize = CGSize(width: view.frame.width, height: 800)
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        if #available(iOS 11, *) {
+//            let safeTop = view.safeAreaInsets.top
+//            let safeBottom = view.safeAreaInsets.bottom
+//            scrollView.frame = CGRect(x: 0, y: safeTop, width: view.frame.width, height: view.frame.height - safeBottom - safeTop)
+//        } else {
+//            let safeTop = topLayoutGuide.length
+//            let safeBottom = bottomLayoutGuide.length
+//            scrollView.frame = CGRect(x: 0, y: safeTop, width: view.frame.width, height: view.frame.height - safeBottom - safeTop)
+//        }
+        
+//    }
 
 }
