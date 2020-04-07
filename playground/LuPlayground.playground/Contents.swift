0a2e19ddb7985b1f7f05c0d02692f4e792b1fd8f
lusir <luzhang316@gmail.com>
2020-04-07 16:29:19 +0800

    Swift05-Auteurs project init

--- playground/LuPlayground.playground/Contents.swift
+++ playground/LuPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var str = "Hello, playground"
+var str: String = "Hello, playground"
+
+let tableView: UITableView = {
+    let tbView = UITableView.init(frame: .zero, style: UITableView.Style.plain)
+    return tbView
+}()
+
+
+class Person: NSObject {
+    @objc var name: String
+    var tags: [String] = []
+    
+    init(pName: String) {
+        name = pName
+    }
+}
+
+let person = Person(pName: "lol")
+
+let name = person.value(forKey: #keyPath(Person.name))
+#keyPath(Person.name)
+let n1 = person.value(forKey: "name")
